d6d5f8d402e17e6714b013c6ddff5ae91eda7610
Akhil353 <asingamneni@gmail.com>
2024-08-09 21:29:34 -0700

    more boilerplate code

--- detection.py
+++ detection.py
@@ -2,14 +2,20 @@ import cv2
 import time
 import subprocess
 import os
+from model import NeuralNetwork
 
 
 while True:
+    # Take an image to be processed by neural network
     time.wait(0.1)
     subprocess.Popen(["fswebcam", "input.jpg"])
 
     # Put detection code here once implemented
+    detector = NeuralNetwork()
+    detector.load_model('model_file_name')
+    action = detector.predict('input.jpg')
 
-    for file in os.listdir('path/to/directory'):
-        if file.startswith('input'):
-            os.remove(file)
+
+    for filename in os.listdir('path/to/directory'):
+        if filename.startswith('input'):
+            os.remove(filename)
