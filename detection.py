744f0128a5c816bf9c88b3277927ad26c72d31d2
Akhil353 <asingamneni@gmail.com>
2024-09-02 16:13:19 -0700

    Added yolo detection

--- detection.py
+++ detection.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29b820a7c1788bbe62d200aeada3d7f44e9d9ad2e273d3c3cb3d8f434259bfa4
-size 511
+oid sha256:d1c68d803ef474507d5639641db05fed289a0ff3c5fd79f062a8fa13a206cea2
+size 609
