6e85e9cec1e0a5f99035002ee49e588ee7c92eae
Akhil353 <asingamneni@gmail.com>
2024-09-02 16:13:19 -0700

    Added yolo detection

--- detection.py
+++ detection.py
@@ -2,20 +2,26 @@ import cv2
 import time
 import subprocess
 import os
-from ultralytics import YOLOv10
+from ultralytics import YOLO
 
 
-while True:
-    # Take an image to be processed by neural network
-    time.wait(0.1)
-    subprocess.Popen(["fswebcam", "input.jpg"])
+model = YOLO("yolov8n.pt")
 
-    # Put detection code here once implemented
-    detector = NeuralNetwork()
-    detector.load_model('model_file_name')
-    action = detector.predict('input.jpg')
+video_path = "video.mp4"
+video = cv2.VideoCapture(0)
 
+while video.isOpened():
+    # Read a frame from the video
+    success, frame = video.read()
 
-    for filename in os.listdir('path/to/directory'):
-        if filename.startswith('input'):
-            os.remove(filename)
+    if success:
+        # Run YOLOv8 tracking on the frame, persisting tracks between frames
+        results = model.track(frame, persist=True)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLOv8 Tracking", annotated_frame)
+        cv2.waitKey(1)
+
