501f148d37041b6a8404dfc5a34e2f1fc652a88d
Akhil353 <asingamneni@gmail.com>
2024-09-03 23:12:18 -0700

    boilerplate distance detection (needs to be fixed)

--- detection.py
+++ detection.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e01979ce87e91991512309b618c780b113aace23e1c21bc2bd17d8bf899c3166
-size 1121
+oid sha256:7b02fbc4b63fe7fa5efd42b0d4d9045a4edd29a36e88b87a9ceff3044140a4fd
+size 2321
