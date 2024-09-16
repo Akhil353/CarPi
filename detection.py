bc1904648fef53ac0982b08b17336eb3ccba2bd9
Akhil353 <asingamneni@gmail.com>
2024-09-15 20:25:35 -0700

    reverting back to original boilerplate code

--- detection.py
+++ detection.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b02fbc4b63fe7fa5efd42b0d4d9045a4edd29a36e88b87a9ceff3044140a4fd
-size 2321
+oid sha256:da274c7246b9beb5d0d07cfde745dfe761a64302d28235541f1e878bf0c18959
+size 887
