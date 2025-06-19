b0092911a512f328a86bfc7e2c82b61aaa711b65
Akhil353 <asingamneni@gmail.com>
2025-06-19 16:29:46 -0700

    updating files to match yolov12

--- First.py
+++ First.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1ade884060da4f62fb4a4acb488dfb76f5069993bd26c9a98b16b614c2309b4
-size 513
+oid sha256:4dbb995c565d6887a1d4b99a6245de9140a3f612715f3294e97fb050d45271aa
+size 517
--- detection.py
+++ detection.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da274c7246b9beb5d0d07cfde745dfe761a64302d28235541f1e878bf0c18959
-size 887
+oid sha256:305d0f4f7524620027fa6b857d84003f4e5152776855a73f9105da31e69ffcc6
+size 888
