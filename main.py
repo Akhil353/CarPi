d4710c41beeafaf283a01505bb34d859c940ef54
Harkirat47 <har.ki.kirat@gmail.com>
2025-08-01 19:03:38 -0700

    much drift shift

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2579aa6006bc2a423cabd2112fcc0c546d0fb1dace68a1d0c9c34701bfc8811
+oid sha256:107afd9ec6d7b0d3faf799eeefff0d925081a451e7f17b3a0abe2a2d40bf5c15
 size 2482
---
+++ motionDetect.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f2a2ba0b6fd63d8d066a150b3c1a416840742e87987e1767e82db6d17c0fb9d8
+size 1514
