bf0149af94a930bed74591981ca6a94e4e1e8ef3
Harkirat47 <har.ki.kirat@gmail.com>
2025-08-01 19:03:38 -0700

    much drift shift

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def main():
 
     model = YOLO("yolo12s.pt")
 
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(1)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
     cap.set(cv2.CAP_PROP_FPS, 30)
