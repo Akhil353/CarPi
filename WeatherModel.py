090a8391460f1c69e9f87b3674de53ad0d34b0aa
Akhil353 <asingamneni@gmail.com>
2025-08-31 15:48:31 -0700

    updating model for high res output

--- WeatherModel.py
+++ WeatherModel.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:860c87e2955ee4d86bce74b7f3adb59d8277e9502c40108705a6e628927c96be
-size 5372
+oid sha256:9dc28f2655d62757d55eec89cc51d2d02ffe25c148ea9c8fe288589f28066527
+size 5783
